f7a28e7311de78c2f1f44df3dceda00143e38212
Mikael Berg <mikael.berg@cognite.com>
2021-05-15 16:43:31 +0200

    Make it easy to deploy client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and saverate to reload.
+          next-level irrigation?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
