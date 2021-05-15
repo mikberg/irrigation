a377dafda95cd0f120c13ddfeae0f08983654d14
Mikael Berg <mikael.berg@cognite.com>
2021-05-15 22:30:12 +0200

    More material design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,50 @@
+import { AppBar, Container, CssBaseline, Grid, IconButton, Paper, Toolbar, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  }
+}));
+
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          next-level irrigation?
-        </p>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="absolute">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="open drawer">
+            <MenuIcon />
+          </IconButton>
+          <Typography component="h1" variant="h6" color="inherit" noWrap>
+            Irrigation
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer}></div>
+        <Container maxWidth="lg">
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={8} lg={9}>
+              <Paper>
+                <p>Hello from paper</p>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
     </div>
   );
 }
