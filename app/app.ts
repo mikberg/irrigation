e695aba452033111366bfd6b72559042f5ed886a
Mikael Berg <mikael.berg@cognite.com>
2021-05-16 15:26:14 +0200

    Simpler app with explicit webpack

--- app/app.ts
+++
@@ -1,4 +0,0 @@
-const el: HTMLDivElement = document.createElement('div');
-el.innerText = 'Hello, s';
-el.className = 'ts1';
-document.body.appendChild(el);
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import './App.css';
-import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          next-level irrigation?
-        </p>
-      </header>
+    <div>
+      Hello from App!?!!!!
     </div>
   );
 }
